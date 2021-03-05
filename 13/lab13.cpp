63934f01f695087ca3e0fc6861b8980a5d3a2bb0
KaoXinRei <igor.kopygin@gmail.com>
2021-03-05 19:03:14 +0500

    board size corrected

--- 13/lab13.cpp
+++ 13/lab13.cpp
@@ -5,88 +5,74 @@ using namespace std;
 
 bool valid(int x, int y, bool** board)
 {
-	for (int i = 0; i < 9; i++)
-	{
-		if (board[x][i])
-		{
-			return false;
-		}
-		if (board[i][y])
-		{
-			return false;
-		}
-	}
-	int delta = x - y;
-	for (int i = max(0,delta); i < min(9, 9+delta); i++)
-	{
-		if (board[i][i-delta])
-		{
-			return false;
-		}
-	}
-	delta = x - 8 + y;
-	for (int i = max(0,delta); i < min(9, 9+delta); i++)
-	{
-		if (board[i][min(x+y,8)-i])
-		{
-			return false;
-		}
-	}
-	return true;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j])
+            {
+                if (x == i || y == j || x + y == i + j || x - y == i - j)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
 }
 
 int place_queens(bool** board, int n)
 {
-	if (n==0)
-	{
-		return 0;
-	}
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			if (valid(i, j, board))
-			{
-				board[i][j] = true;
-				n -= 1;
-				n = place_queens(board, n);
-				if (n == 0)
-				{
-					return 0;
-				}
-				board[i][j] = false;
-				n += 1;
-			}
-		}
-	}
-	return n;
+    if (n==0)
+    {
+        return 0;
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (valid(i, j, board))
+            {
+                board[i][j] = true;
+                n -= 1;
+                n = place_queens(board, n);
+                if (n == 0)
+                {
+                    return 0;
+                }
+                board[i][j] = false;
+                n += 1;
+            }
+        }
+    }
+    return n;
 }
 
 int main()
 {
-	bool** board = new bool*[9];
-	for (int i = 0; i < 9; i++)
-	{
-		board[i] = new bool[9];
-		for (int j = 0; j < 9; j++)
-		{
-			board[i][j] = false;
-		}
-	}
-	place_queens(board, 8);
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			if (board[i][j])
-			{
-				cout << "* ";
-			}
-			else
-			{
-				cout << "0 ";
-			}
-		}
-		cout << endl;
-	}
+    bool** board = new bool*[8];
+    for (int i = 0; i < 8; i++)
+    {
+        board[i] = new bool[8];
+        for (int j = 0; j < 8; j++)
+        {
+            board[i][j] = false;
+        }
+    }
+    place_queens(board, 8);
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j])
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
 }
